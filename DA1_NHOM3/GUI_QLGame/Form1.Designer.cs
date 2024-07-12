bb4664990b17d8ebf5d5abad272073256d0f42ce
baovannguyen <baoquy1400@gmail.com>
2024-07-12 17:21:21 +0700

    hhhhhhhhhhh

--- DA1_NHOM3/GUI_QLGame/Form1.Designer.cs
+++ DA1_NHOM3/GUI_QLGame/Form1.Designer.cs
@@ -39,7 +39,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(498, 148);
+            this.button1.Location = new System.Drawing.Point(318, 57);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
@@ -94,7 +94,7 @@
             // 
             // button6
             // 
-            this.button6.Location = new System.Drawing.Point(465, 181);
+            this.button6.Location = new System.Drawing.Point(285, 90);
             this.button6.Name = "button6";
             this.button6.Size = new System.Drawing.Size(75, 23);
             this.button6.TabIndex = 6;
